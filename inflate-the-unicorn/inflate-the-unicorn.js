9e44ecc3a2989b15aa200b69ffc9e3d0ea81574c
Christelle Quilang <christelle.quilang@protonmail.com>
2022-05-14 16:16:45 +1200

    Create function that gets called whenever a unicorn is clicked

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,18 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+// DONE! create a function that gets called every time you click on one of the unicorns
+
+let unicorns = document.getElementsByClassName('inflate-an-image')
+
+for (let i = 0; i < unicorns.length; i++) {
+  unicorns[i].onclick = unicornClicked
+}
+
+function unicornClicked(e) {
+  let image = e.target
+  console.log('hey!', image)
+}
