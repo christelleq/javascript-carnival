e2cda1a0e3c57b03f212390fe19cc7f5cebe0100
Christelle Quilang <christelle.quilang@protonmail.com>
2022-05-14 17:37:43 +1200

    Add cells variable

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -4,6 +4,9 @@
 
 console.log('Whack-a-Mole!')
 
-// The ability to store or get references to the cells in the HTML table.
+// DONE! The ability to store or get references to the cells in the HTML table.
+
+let cells = document.getElementsByTagName('td')
+
 // A function to randomly select which cell should show the mole.
 // A way to show a mole in the chosen cell.
