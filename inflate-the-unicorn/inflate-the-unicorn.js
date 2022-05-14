ba4f4947b146de0c3ce60de2cb3484a2fb9a34f6
Christelle Quilang <christelle.quilang@protonmail.com>
2022-05-14 17:13:02 +1200

    Delete console.logs

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -10,13 +10,11 @@ let unicorns = document.getElementsByClassName('inflate-an-image')
 
 for (let i = 0; i < unicorns.length; i++) {
   unicorns[i].onclick = unicornClicked
-  console.log(unicorns[i])
 }
 
 function unicornClicked(e) {
   let image = e.target
   // DONE! when you click the button your function should change the image of the unicorn to one of the other images
-  console.log('hey!', image)
 
   if (image.src.match('./images/unicorn-0.png')) {
     image.src = './images/unicorn-1.png'
