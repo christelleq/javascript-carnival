d17d98091c1370bc887386763e0d8f409d50637f
Christelle Quilang <christelle.quilang@protonmail.com>
2022-05-14 17:12:15 +1200

    Change unicorns when clicked

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -10,9 +10,22 @@ let unicorns = document.getElementsByClassName('inflate-an-image')
 
 for (let i = 0; i < unicorns.length; i++) {
   unicorns[i].onclick = unicornClicked
+  console.log(unicorns[i])
 }
 
 function unicornClicked(e) {
   let image = e.target
+  // DONE! when you click the button your function should change the image of the unicorn to one of the other images
   console.log('hey!', image)
+
+  if (image.src.match('./images/unicorn-0.png')) {
+    image.src = './images/unicorn-1.png'
+    console.log('1')
+  } else if (image.src.match('./images/unicorn-1.png')) {
+    image.src = './images/unicorn-2.png'
+    console.log('unicorn 2')
+  } else if (image.src.match('./images/unicorn-2.png')) {
+    image.src = './images/unicorn-3.png'
+    console.log('unicorn 3')
+  }
 }
