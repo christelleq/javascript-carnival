5f2a8282bf66f996948655c9e3ed7f79bce4ce0c
Christelle Quilang <christelle.quilang@protonmail.com>
2022-05-15 16:29:32 +1200

    Get left and right keys to change clown head

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -9,8 +9,27 @@ console.log('Dress The Clown!')
 let headIndex = 0
 
 function changeClownHead() {
-  headImage = './images/head' + headIndex + '.png'
-  document.getElementById('head').src = headImage
+  document.addEventListener('keydown', function (e) {
+    switch (e.key) {
+      case 'ArrowRight':
+        if (headIndex >= 5) {
+          headIndex = 0
+        } else {
+          headIndex++
+        }
+        break
+      case 'ArrowLeft':
+        if (headIndex <= 0) {
+          headIndex = 5
+        } else {
+          headIndex--
+        }
+        break
+    }
+    console.log(`headIndex: ${headIndex}`)
+    headImage = './images/head' + headIndex + '.png'
+    document.getElementById('head').src = headImage
+  })
 }
 
 changeClownHead()
