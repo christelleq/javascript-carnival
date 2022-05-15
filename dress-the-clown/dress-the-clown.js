080b3fff469beed1853b08ece6623267826d9c58
Christelle Quilang <christelle.quilang@protonmail.com>
2022-05-15 17:19:41 +1200

    Delete commented out code

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -4,34 +4,6 @@
 
 console.log('Dress The Clown!')
 
-// // change clown head
-
-// let headIndex = 0
-
-// function changeClownHead() {
-//   document.addEventListener('keydown', function (e) {
-//     switch (e.key) {
-//       case 'ArrowRight':
-//         headIndex = rightArrow(headIndex)
-//         break
-//       case 'ArrowLeft':
-//         headIndex = leftArrow(headIndex)
-//         break
-//     }
-//     console.log(`headIndex: ${headIndex}`)
-//     headImage = './images/head' + headIndex + '.png'
-//     document.getElementById('head').src = headImage
-//   })
-// }
-
-// changeClownHead()
-
-// // change clown body
-// function changeClownBody() {}
-
-// // change clown shoes
-// function changeClownShoes() {}
-
 let clothingIndex = 0
 
 document.addEventListener('keydown', function (e) {
