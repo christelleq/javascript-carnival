250eb9a1de2be21b66604076221947e439db8e98
Christelle Quilang <christelle.quilang@protonmail.com>
2022-05-15 14:37:02 +1200

    Add stub function for changing head, body and shoes

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -5,7 +5,10 @@
 console.log('Dress The Clown!')
 
 // change clown head
+function changeClownHead() {}
 
 // change clown body
+function changeClownBody() {}
 
 // change clown shoes
+function changeClownShoes() {}
