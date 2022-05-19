ac78c0959da44967371a0bdcb3c9006235f92381
Christelle Quilang <christelle.quilang@yahoo.co.nz>
2022-05-20 09:29:34 +1200

    Add stretch pseudocode

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -46,6 +46,8 @@ function playSound() {
   audio.play()
 }
 
+// DONE! Add a `counter` to the page that counts how many times you have whacked the mole!
+
 let whackCount = 0
 
 function countWhacks() {
@@ -53,3 +55,9 @@ function countWhacks() {
   document.getElementById('whackCount').innerHTML = `Whack Count: ${whackCount}`
   console.log(`whackCount: ${whackCount}`)
 }
+
+// Add a `timer` to the page that gives you 10 seconds to whack as many moles as possible, and then pops up your score as a Javascript alert.
+
+// Add a `high score` field that stores your highest score and replaces it if a new, higher score is achieved. _*Note:_ for now, this score will be reset when you refresh your browser. For it to last, you would need some form of _database_, which we  cover later during bootcamp.
+
+// Rewrite your `random index` function so it can't give you the same index twice in a row.
